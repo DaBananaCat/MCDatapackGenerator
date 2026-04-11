3064445293dd69b4bccca3f81d25cb9f54c1b47b
Sir_BananaCat <130233065+DaBananaCat@users.noreply.github.com>
2026-04-11 16:30:22 -0500

    Give item functions
    When an item is made, a function is made with it as a give command to obtain it without crafting

--- MCIM_MakeData.py
+++ MCIM_MakeData.py
@@ -5,18 +5,31 @@ import asyncio
 import pprint
 import shutil
 
+# Vanilla folder (Obtain from .minecraft/versions/{version})
 vanilla_folder = "1.21.11/"
+
+# Replace with the location you want the datapack and resourcepack to output
+# You may want to have them in their respective folders for easier testing
 datpack_folder = "C:/Users/" + os.getlogin() + "/AppData/Roaming/ModrinthApp/profiles/Main/datapacks/Food"
 resourcepack_folder = "C:/Users/" + os.getlogin() + "/AppData/Roaming/ModrinthApp/profiles/Main/resourcepacks/Food"
+
+# Internal name, may cause issues if anyother datapack has the same one
 namespace = "food"
 
+"""
+Until main(), the following code does not
+need to be touched under practical use
+"""
 
+# Prepare a list of the Minecraft models that will be changed
 models_modded = {}
 
 class Item():
     def __init__(self, id, display_name, components, texture, item_to_replace="minecraft:firework_star"):
         self.id = id
         self.display_name = display_name
+
+        # Append the item's name to the components
         name_components = {"custom_name": {"italic":False,"text":display_name},
                            "custom_model_data":{"strings":[id]}
                            }
@@ -24,6 +37,10 @@ class Item():
         self.texture = texture
         self.base_item = item_to_replace
 
+        # Write a function of give command of the item
+        write_mcfunction(datpack_folder+"/data/" + namespace + "/function/give_" + id + ".mcfunction", data_to_command(item_to_replace,self.components))
+
+        # Write model
         write_json(resourcepack_folder + "/assets/" + namespace + "/item/" + id + ".json", 
                    {
             "model": {
@@ -39,9 +56,12 @@ class Item():
             }
         })
 
+        # If this model has not been touched yet, mark it as modified
         if item_to_replace not in models_modded:
             model = read_json(vanilla_folder + "/assets/minecraft/items/" + item_to_replace[10:] +".json")
-            
+            models_modded[item_to_replace] = model
+
+        # If this model has been modified, append changes instead of replacing
         else:
             model = models_modded[item_to_replace]
             pprint.pp(model)
@@ -66,28 +86,28 @@ class Item():
                         "model": "food:item/" + id
                     }
                 })
-        if item_to_replace not in models_modded:
-            models_modded[item_to_replace] = model
-        else:
-            print("new model is:")
-            pprint.pp(model)
+            
         write_json(resourcepack_folder + "/assets/minecraft/items/" + item_to_replace[10:] + ".json", model)
 
+        # Put the specified item's texture into the resourcepack
         duplicate("assets/"+texture, resourcepack_folder + "/assets/" + namespace + "/textures/item/" + texture)
 
 def duplicate(source_path, destination_path):
     shutil.copy2(source_path, destination_path)
 
+# Get the data of a JSON
 def read_json(path):
     with open(path) as f:
         return json.load(f)
 
+# Write to a new JSON
 def write_json(path, data):
     os.makedirs(os.path.dirname(path), exist_ok=True)
     with open(path, "w") as f:
         json.dump(data, f, indent=4)
 
-
+# Get a dictionary of every json in a specific folder
+# Useful for mass-modifying
 def load_all_json(folder):
     files = {}
     for root, _, names in os.walk(folder):
@@ -96,7 +116,7 @@ def load_all_json(folder):
             files[path] = read_json(path)
     return files
 
-
+# Add components to a vanilla recipe's result
 def modify_recipes(recipes, item_components):
     for path, data in recipes.items():
         try:
@@ -106,6 +126,7 @@ def modify_recipes(recipes, item_components):
         except:
             pass
 
+# Write components to a vanilla loot table's drops
 def write_loot_data(entry, item_components):
     item_id = entry["name"]
     if item_id in item_components:
@@ -124,29 +145,49 @@ def write_loot_data(entry, item_components):
                         "components": item_components[item_id],
                     }
                 ]
+        
+# Uhhh I don't remember what I did here
 def modify_loot_tables(loot_tables, item_components):
     for path, data in loot_tables.items():
-        if True: #path[-(len("oak_leaves.json")):] == "oak_leaves.json":
-            try:
-                pools = 0
-                for pool in data["pools"]:
-                    pools += 1
-                    try:                    
-                        for entry in pool["entries"]:
-                            if entry["type"] == "minecraft:item" or entry["type"] == "minecraft:alternatives": 
-                                if "children" in entry.keys():
-                                    for child in entry["children"]:
-                                        write_loot_data(child, item_components)
-                                else:    
-                                    write_loot_data(entry, item_components)
-                    except:
-                        pass
-            except:
-                pass
+        try:
+            pools = 0
+            for pool in data["pools"]:
+                pools += 1
+                try:                    
+                    for entry in pool["entries"]:
+                        if entry["type"] == "minecraft:item" or entry["type"] == "minecraft:alternatives": 
+                            if "children" in entry.keys():
+                                for child in entry["children"]:
+                                    write_loot_data(child, item_components)
+                            else:    
+                                write_loot_data(entry, item_components)
+                except:
+                    pass
+        except:
+            pass
 
-def make_recipe(name: str, type: str, result: Item, result_amount: int, ingredients: list, pattern=[], category:str = "misc", group:str = None):
+def write_mcfunction(path, contents):
+    with open(path, "w+") as f:
+        if type(contents) == list:
+            for command in contents:
+                f.write(command)
+        else:
+            f.write(contents)
+
+def data_to_command(item, components):
+    result = "give @s " + item + "["
+    for key,val in components.items():
+        result += key + "=" + str(val) + ","
+
+    return result[:-1] + "]"
+            
+
+# Write a new custom recipe
+def make_recipe(name: str, type: str, result, result_amount: int, ingredients: list, pattern=[], category:str = "misc", group:str = None):
     recipe = {}
     path = datpack_folder + "/data/" + namespace + "/recipe/" + name + ".json"
+    
+    # Set the recipe
     if type == "shapeless":
         recipe["type"] = "minecraft:crafting_shapeless"
         recipe["ingredients"] = ingredients
@@ -166,20 +207,14 @@ def make_recipe(name: str, type: str, result: Item, result_amount: int, ingredie
     }
     write_json(path, recipe)
 
-
-def new_item(id, components, recipes):
-    path = datpack_folder + "/data/" + namespace + "/"
-
-    for name, data in recipes:
-        write_json(path + "recipe/"+name+".json", data)
-
+# Example code
 def main():
 
     Dough = Item("dough", "Dough", {}, "dough.png")
     make_recipe("craft_dough", "shapeless", Dough, 2, ["minecraft:wheat"], group = "dough")
 
     Bleh = Item("bleh", "bleh", {}, "bleh.png")
-    make_recipe("craft_bleh", "shapeless", Bleh, 2, ["minecraft:diamond"], group = "bleh")
+    make_recipe("craft_bleh", "shaped", Bleh, 2, {"x": "minecraft:dirt", "z": "minecraft:copper_door"}, pattern=["xxx","xxx","zxz"], group = "bleh")
 
 if __name__ == "__main__":
     main()
